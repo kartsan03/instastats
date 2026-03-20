9610de539e27b1e6c584531a9ad0d6849a77116c
kartsan03 <kartsan03@gmail.com>
2026-03-20 07:16:23 +0100

    Add interactive choices for disks. Left placeholders to fill later.

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def printer_banner():
     print(banner)
 
 printer_banner()
-print("Welcome in instastats")
+print("Welcome to instastats")
 print("You can easily monitor your system with this tool")
 
 
@@ -23,11 +23,25 @@ for disk in all_disks:
     list_of_system_disks.append(disk.device)
 
 print("Please choose the disk you want to see information about:")
-print("|| 0 General Information about your system")
+print("|| 0  General information about your system")
 
 for i, device in enumerate(list_of_system_disks, start=1):
     print(f"|| {i}  {device}")
 
+try:
+    user_choice = int(input("Enter your choice: "))
+    if 0 <= user_choice <= (len(list_of_system_disks)) :
+        if user_choice == 0:
+            print("PLACEHOLDER TO FUTURE THINGY")
+        if user_choice > 0:
+            disk_index = user_choice - 1
+            print(f"PLACEHOLDER TO FUTURE THINGY {list_of_system_disks[disk_index]} END OF THINGY")
+    else:
+        print("Please enter number from list above. Exiting program.")
+
+
+except ValueError:
+    print("Invalid input. Please enter a number. Exiting program.")
 # total, used, free = shutil.disk_usage('/')
 # print(f'Total: {total / (2**30)} GiB')
 # print(f'Used: {used / (2**30)} GiB')
